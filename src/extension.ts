c11a483bc0ad884db38957da068fadd11bca96a3
Vincenzo Manto <vincenzo.manto@sys-datgroup.com>
2022-11-09 15:06:32 +0100

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	// Register the Sidebar Panel
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"darkside-sidebar",
+			sidebarProvider
+		)
+	);
+
+	
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,5 @@
+<script>
+    const imgUrl = new URL('./darth-vader.png', import.meta.url).href
+</script>
+<h1>Sidebar Panel</h1>
+<img src="images/luke.gif" alt="background image">
